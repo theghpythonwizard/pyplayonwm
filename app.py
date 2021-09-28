fb213546f3d3b32295a9403bcad610986cdc52bd
Jason Schultheis <theghpythonwizard@github.com>
2021-09-27 21:30:42 -0700

    updating var name

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ def run_cmd(file_path, episode_name, output_dir):
             episode_name,
             output_dir
         )
-    rc = i.run()
-    return rc
+    output = i.run()
+    return output
 
 if __name__ == '__main__':
     fp = '/home/jasschul/share/tvshows/tvshow_files/The_Office/Season_3'
     output_path = '/home/jasschul/tvoutput'
-    # rc = run_cmd(fp, "E01_Gay_Witch_Hunt_S03E01.mp4", output_path)
-    # print(rc)
+    # output = run_cmd(fp, "E02_The_Convention_S03E02.mp4", output_path)
+    # print(output)
     for episode in os.listdir(fp):
         try:
-            rc = run_cmd(fp, episode, output_path)
-            print(rc)
+            output = run_cmd(fp, episode, output_path)
+            print(output)
         except KeyboardInterrupt:
             sys.exit(1)
